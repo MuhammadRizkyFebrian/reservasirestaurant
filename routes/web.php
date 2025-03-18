f677f2e83587de8a419df8967283bc215e7d17b7
FeatherineIVE <hakimnaufal073@gmail.com>
2025-03-18 23:20:41 +0700

    Migrasi Proyek PBL(3312401088)

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
