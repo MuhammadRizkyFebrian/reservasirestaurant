39626c61ed66b0a11a8b63d2e8bd0ca5ef6e3304
sakila161105 <nandasakila1605@gmail.com>
2025-03-24 14:06:11 +0700

    migrasi proyek pbl list reservasi

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ListReservasiController;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -12,3 +13,6 @@ Route::get('/welcome', function () {
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/register', [RegisterController::class, 'show'])->name('register');
+Route::get('/listereservasi/{id}/{nama}/{tanggal}/{jumlah_orang}', 
+    [ListReservasiController::class, 'show']
+)->name('list.reservasi.detail');
