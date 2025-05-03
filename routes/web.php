f7383e815fc70bd0ca0e3d2dcde381826faf34cf
yudaahscwlth <raflesyudaa@gmail.com>
2025-05-03 14:02:51 +0700

    View Login, Register, Lupa Password. Rafles Yuda Stevenses Nababan - 3312401062

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,3 +26,17 @@ Route::get('/admin/pelanggan', function () {
 Route::get('/admin/pemesanan', function () {
     return view('pages.dashboard admin.pemesanan');
 });
+
+
+// Route autentikasi
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route registrasi
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+// Route reset password
+Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
